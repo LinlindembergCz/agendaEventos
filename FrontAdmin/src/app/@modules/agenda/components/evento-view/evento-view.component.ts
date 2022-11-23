4425dedad1ddbe59f8a729e70a26ece19c92bffb
STEFANINI-DOM\lcaraujo1 <lcaraujo1@latam.stefanini.com>
2022-11-22 21:18:03 -0300

    ajustes para responsividade

--- FrontAdmin/src/app/@modules/agenda/components/evento-view/evento-view.component.ts
+++ FrontAdmin/src/app/@modules/agenda/components/evento-view/evento-view.component.ts
@@ -7,7 +7,8 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 import { SafeResourceUrl } from '@angular/platform-browser';
 
 import { Meses } from '../../../../@core/enums/ListaMeses';
-import { FileService } from 'src/app/@shared/services/file.service';
+import { FileService } from '../../../..//@shared/services/file.service';
+import { ApplicationStateService } from '../../../../@bootstrap/services/application-state.service';
 
 class EventolabModel {
   id: string;
@@ -44,6 +45,10 @@ export class EventoViewComponent implements OnInit , AfterViewInit
   evento: EventolabModel=new EventolabModel();
   picture: SafeResourceUrl;
 
+  isDesktop: boolean = false;
+  isMobile: boolean = false;
+  isTablet: boolean = false;
+
   constructor(
     private http: RequestPromiseService,
     private router: Router,
@@ -51,40 +56,51 @@ export class EventoViewComponent implements OnInit , AfterViewInit
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
     private fileService: FileService,
+    private applicationStateService: ApplicationStateService,
   ) { }
 
   ngOnInit(): void 
   {
+
+    this.isDesktop = this.applicationStateService.device().isDesktop();
+    this.isMobile =  this.applicationStateService.device().isMobile();
+    this.isTablet = this.applicationStateService.device().isTablet();
+    
+
       this.route.queryParams.subscribe(params => {
                                        if (params['id'])
                                        {
                                           this.http.get<Eventolab>(environment.services.api,
-                                          `${environment.routes.eventoSebraeLab.root}/${params['id']}`).then( e=> {  
-                                            
-                                            this.evento = {...e};
-
-                                            let startEvent: any = e.dias[0];//primeiro evento
-                                            let endEvent: any = e.dias[e.dias.length - 1];//ultimo evnto               
-                                            let startDay : string = startEvent?.data;//data inicial
-                                            let endDay: string = endEvent?.data;//data final  
-
-                                            this.evento.horainicial= startEvent?.horainicio;
-                                            this.evento.horafinal= endEvent?.horafim;
-                                            this.evento.data = startDay;
-                                            this.evento.datainicial=  startDay?.substring(8,10); //primeiro dia
-                                            this.evento.datafinal= endDay?.substring(8,10); //ultimo dia
-                                            this.evento.qtdDias = e.dias.length;
-
-                                            this.download(this.evento.id);                                          
-                                          });
+                                          `${environment.routes.eventoSebraeLab.root}/${params['id']}`).then( e=> 
+                                            {                                              
+                                                this.evento = {...e};
+                                                this.fillDataHoraInicialFinal( e );
+                                                this.download(this.evento.id);                                          
+                                            });
                                         }
                                         else
                                         if (params['data'])
                                         {
-                                           this.evento = JSON.parse(params['data'])
+                                           this.evento = JSON.parse(params['data'])                         
+                                           this.fillDataHoraInicialFinal( this.evento );                                          
                                            this.download(this.evento.id);                                          
                                         }
                                     });
+  }
+
+  fillDataHoraInicialFinal( e: any)
+  {
+      let startEvent: any = e.dias[0];//primeiro evento
+      let endEvent: any = e.dias[e.dias.length - 1];//ultimo evnto               
+      let startDay : string = startEvent?.data;//data inicial
+      let endDay: string = endEvent?.data;//data final  
+
+      this.evento.horainicial= startEvent?.horainicio;
+      this.evento.horafinal= endEvent?.horafim;
+      this.evento.data = startDay;
+      this.evento.datainicial=  startDay?.substring(8,10); //primeiro dia
+      this.evento.datafinal= endDay?.substring(8,10); //ultimo dia
+      this.evento.qtdDias = e.dias.length;
   }
 
   ngAfterViewInit()
@@ -121,4 +137,13 @@ export class EventoViewComponent implements OnInit , AfterViewInit
   { 
     return Meses[new Date(data).getMonth()]
   }
+
+
+  canPublish(): boolean
+  {
+   
+    return true// (this.evento.status=='Rascunho') && (this.evento.publicaosite)
+  }
+
+
 }
